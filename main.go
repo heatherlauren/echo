86ff3e98392d40e508cd0b709acdffe6d4243f43
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-27 18:12:19 +0000

    have html and custom header returning correctly

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/labstack/echo"
 )
 
+type Payload struct {
+		Secret string `json:"secret"`
+}
+
 func main() {
 	e := echo.New()
 	e.POST("/hello/:name", postHandler)
@@ -13,9 +17,16 @@ func main() {
 }
 
 func postHandler(c echo.Context) error {
-	c.Response().Header().Add("X-Secret", "shotsfired")
+	payload := new(Payload)
+
+	if err := c.Bind(payload); err != nil {
+		return err
+	}
+
+	secret := payload.Secret
+
+	c.Response().Header().Add("X-Secret", secret)
 
 	name := c.Param("name")
-
 	return c.HTML(http.StatusOK, "<h1>Hello " + name + "</h1>")
 }
