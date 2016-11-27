09daa64dd5f22931387390da87c415ebb9eed773
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-27 12:07:53 +0000

    have postHandler returning varying HTML

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", getHandler)
+	e.POST("/hello/:name", postHandler)
 	e.Logger.Fatal(e.Start(":1337"))
 }
 
-func getHandler(c echo.Context) error {
+func postHandler(c echo.Context) error {
 	c.Response().Header().Add("X-Secret", "shotsfired")
-	return c.HTML(http.StatusOK, "<h1>Hello jonny</h1>")
+
+	name := c.Param("name")
+
+	return c.HTML(http.StatusOK, "<h1>Hello " + name + "</h1>")
 }
