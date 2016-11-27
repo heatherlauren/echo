b9183fdbc5e04695de6f597aa3e515c059d67d80
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-27 20:26:55 +0000

    add working & formatted main.go

--- main.go
+++ main.go
@@ -6,10 +6,27 @@ import (
 	"github.com/labstack/echo"
 )
 
+type Payload struct {
+	Secret string `json:"secret"`
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.POST("/hello/:name", postHandler)
 	e.Logger.Fatal(e.Start(":1337"))
+}
+
+func postHandler(c echo.Context) error {
+	payload := new(Payload)
+
+	if err := c.Bind(payload); err != nil {
+		return err
+	}
+
+	secret := payload.Secret
+
+	c.Response().Header().Add("X-Secret", secret)
+
+	name := c.Param("name")
+	return c.HTML(http.StatusOK, "<h1>Hello "+name+"</h1>")
 }
